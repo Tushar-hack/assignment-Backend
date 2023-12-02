878657d7bae94b623d3ee6e328e8d7d4815064ec
tushartak <tushartak861@gmail.com>
2023-12-02 22:12:32 +0530

    Added Morgan for logging

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 import bodyParser from "body-parser";
 import {PORT} from './config/config.js';
 import {connect} from './config/dbConfig.js';
@@ -9,7 +10,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use('/api/v1', v1ApiRoutes);
 
 app.listen(PORT, async () => {
