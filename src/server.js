6283ab5312724a734c1b9310201799f718ba3ff4
tushartak <tushartak861@gmail.com>
2023-12-02 22:26:05 +0530

    Added Helmet for Security

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import helmet from 'helmet';
 import bodyParser from "body-parser";
 import {PORT} from './config/config.js';
 import {connect} from './config/dbConfig.js';
@@ -10,7 +11,10 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(helmet());
+
 app.use('/api/v1', v1ApiRoutes);
 
 app.listen(PORT, async () => {
